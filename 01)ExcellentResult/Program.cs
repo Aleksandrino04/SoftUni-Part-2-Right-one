c4302f9410e86ff80fdf31a209ee1d389cd31a7c
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-14 23:27:42 +0300

    Add files via upload

---
+++ 01)ExcellentResult/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01_ExcellentResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = (Console.ReadLine());
+
+            if (x == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else if (x != "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Wrong password!");
+
+
+
+            }
+        }   }   }
---
+++ 01. Sum Seconds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime= int.Parse(Console.ReadLine());
+            int totalTime = firstTime + secondTime + thirdTime;
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+           if (minutes < 10) 
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+            
+            else
+            {
+                Console.WriteLine($"{minutes}:{minutes}");
+            }
+
+
+
+        }
+    }
+}
